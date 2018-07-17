739b6547196f6b0887660f1694c7bf2b53420756
William Benton <self@willbenton.com>
2018-07-16 22:30:50 -0400

    WIP on app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, redirect, request, url_for
 import base64
-from cPickle import loads as cPloads
+from pickle import load as cPload
+from pickle import loads as cPloads
 import numpy
 import cloudpickle
+import sys
+import os
 
 app = Flask(__name__)
 
@@ -25,40 +28,24 @@ def index():
     else:
         return "Make a prediction by POSTing to /predict"
 
-@app.route('/model', methods=['POST', 'GET'])
-def new_model():
-    error = None
-    
-    if request.method == 'POST':
-        if app.model is not None:
-            error = "Error:  You've already installed a model"
-        elif 'validator' in request.form and 'predictor' in request.form:
-            val = cPloads(base64.b64decode(request.form['validator']))
-            pred = cPloads(base64.b64decode(request.form['predictor']))
-
-            app.model = Model(val, pred)
-        else:
-            error = "Error:  I need a validator and a predictor"
-    
-    if error is not None:
-        return error
-    elif app.model is None:
-        return "Install a model by POSTing to /model"
-    else:
-        return "Make a prediction by POSTing to /predict"
-
 @app.route('/predict', methods=['POST'])
 def predict():
     import json
     args = cPloads(base64.b64decode(request.form['args']))
     try:
         return json.dumps(app.model(args))
-    except ValueError, ve:
-        return str(ve)
-    except Exception, e:
-        print str(e)
-
+    except ValueError as ve:
+        return str(ve), 400
+    except Exception as e:
+        return str(e), 400
 
 if __name__ == '__main__':
-    app.logger.setLevel(0)
-    app.run(host='0.0.0.0', port=8080)
+  try:
+    with open(os.getenv("S2I_MODEL_PATH"), "rb") as f:
+      (val, pred) = cPload(f)
+      app.model = Model(val, pred)
+  except Exception as e:
+    print(str(e))
+    sys.exit()
+  app.logger.setLevel(0)
+  app.run(host='0.0.0.0', port=8080)
