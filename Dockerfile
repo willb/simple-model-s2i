ada436037fdd632db7be00cfa9452460dec5cd60
William Benton <self@willbenton.com>
2018-10-09 10:47:52 -0500

    checkpoint for demo

--- Dockerfile
+++ Dockerfile
@@ -24,12 +24,13 @@ RUN INSTALL_PKGS="python3 python3-devel python3-setuptools python3-pip python3-v
         dnf -y --setopt=tsflags=nodocs install $INSTALL_PKGS && \
         dnf -y clean all --enablerepo='*'&& \
         pip3 install "cloudpickle == 0.5.3" && \
-        pip3 install nbconvert nbformat && \
+        pip3 install nbconvert nbformat jupyter_client && \
         pip3 install -r /opt/sms/requirements.txt && \
+        pip3 install virtualenv && \
         rm /opt/sms/requirements.txt
 
+RUN chmod 777 /opt/sms /opt/sms/ /opt/sms/*
 RUN chmod 755 /opt/sms/app.py
-RUN chmod 777 /opt/sms/
 RUN chown 185 /opt/sms
 
 EXPOSE 8080
--- extract-requirements.py
+++ extract-requirements.py
@@ -10,12 +10,9 @@ if len(sys.argv) != 3:
 
 nb = nbformat.read(sys.argv[1], nbformat.NO_CONVERT)
 
-cell = nbformat.v4.new_code_cell(""" 
-import cloudpickle
-validator = "validator" in globals() and globals()["validator"] or (lambda x: True)
-with open("model.pickle", "wb") as f:
-    f.write(cloudpickle.dumps((validator, globals()["predictor"])))
+nb.cells = [cell for cell in nb.cells if cell['cell_type'] == 'code' and 'requirements' in cell['source']]
 
+cell = nbformat.v4.new_code_cell(""" 
 with open("extra-requirements.txt", "w") as f:
     reqs = "requirements" in globals() and globals()["requirements"] or []
     f.write('\\n'.join(["%s==%s" % (k,v) for k,v in reqs]))
